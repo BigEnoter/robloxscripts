64589977eb05680531045d708242180d41c214d2
MysticD3AD <30682549+MysticD3AD@users.noreply.github.com>
2019-12-01 09:31:56 +1100

    Delete RemoteSpy_Console.lua

--- RemoteSpy_Console.lua
+++
@@ -1,9 +0,0 @@
-_G.RemoteEvents = true
-_G.RemoteFunctions = true
-_G.Bindables = false
-_G.ignore = {
-             "CharacterSoundEvent", --// you can add up more just add " on left side and " on right side.
-            }
-_G.output = warn
-
-loadstring(game:HttpGet('https://pastebin.com/raw/meTir8qE',true))()
