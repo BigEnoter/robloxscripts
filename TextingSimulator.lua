c326677c7113793dbee671ec8a703346d22afd54
MysticD3AD <30682549+MysticD3AD@users.noreply.github.com>
2019-12-01 12:26:49 +1100

    Create TextingSimulator.lua

---
+++ TextingSimulator.lua
@@ -0,0 +1,64 @@
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+print("To use the autofarm press b to start and then press b again to stop it!")
+warn("To use the autofarm press b to start and then press b again to stop it!")
+
+
+
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+local looping = false
+mouse.KeyDown:connect(function(key)
+
+if key:lower() == "b" or key:upper() == "B" then
+print("bool inverted")
+looping = not looping
+if(looping) then
+print("going into while loop")
+while looping do wait(0.001)
+if(looping == false) then
+print("while loop broken")
+break
+end
+print("ReplicatedStorage event fired")
+game:GetService('ReplicatedStorage').Events.SendTexts:FireServer("Phone")
+end
+end
+end
+end)
