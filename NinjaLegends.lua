9a076e460ae7cca3ba0dc3691e6eb4a98f7e07bf
MysticD3AD <30682549+MysticD3AD@users.noreply.github.com>
2019-12-03 17:42:22 +1100

    Update NinjaLegends.lua

--- NinjaLegends.lua
+++ NinjaLegends.lua
@@ -1,3 +1,8 @@
+local h = Instance.new("Message")
+h.Parent = Workspace
+h.Text = "Script provided by MysticD3AD"
+wait(6)
+h:Remove()
 local player = game.Players.LocalPlayer
 local library = loadstring(game:HttpGet("https://pastebin.com/raw/eWKgbdix",true))()
 library.options.underlinecolor = "rainbow"
