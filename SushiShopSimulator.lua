e5ceebee404c5d10936d8cba81c10362b3271dab
MysticD3AD <30682549+MysticD3AD@users.noreply.github.com>
2019-12-03 17:43:35 +1100

    Update SushiShopSimulator.lua

--- SushiShopSimulator.lua
+++ SushiShopSimulator.lua
@@ -1,1 +1,6 @@
+local h = Instance.new("Message")
+h.Parent = Workspace
+h.Text = "Script provided by MysticD3AD"
+wait(6)
+h:Remove()
 loadstring(game:HttpGet("https://pastebin.com/raw/c6GqDZu9", true))()
