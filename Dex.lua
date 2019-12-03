a616a80377cfdf36674249c88a532f37ff5d4d81
MysticD3AD <30682549+MysticD3AD@users.noreply.github.com>
2019-12-03 17:41:07 +1100

    Update Dex.lua

--- Dex.lua
+++ Dex.lua
@@ -1,1 +1,7 @@
+local h = Instance.new("Message")
+h.Parent = Workspace
+h.Text = "Script provided by MysticD3AD"
+wait(6)
+h:Remove()
+
 loadstring(game:GetObjects('rbxassetid://2180084478')[1].Source)()
